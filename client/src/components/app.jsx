532f26a95f3ef7fd7a9fb7d4a907ce6d39b7ce78
charles-spinosa <charles.spinosa@gmail.com>
2019-03-28 15:47:10 -0700

    [ Dev ] Initial file config established

---
+++ client/src/components/app.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+  render() {
+    return <div>Hello from React</div>;
+  }
+}
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+  entry: './client/src/index.jsx',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, '/client/dist')
+  }
+};
